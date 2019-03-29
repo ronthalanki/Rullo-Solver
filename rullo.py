90504d0ebb28d7e3dc7b0ed98ea459aaad539a66
Sairanjth Thalanki <sthalanki@berkeley.edu>
2019-03-28 18:53:13 -0700

    non-square functionality

--- rullo.py
+++ rullo.py
@@ -3,27 +3,24 @@ from combination import combination_finder
 
 def input(puzzle_name):
     file = open("test-cases/" + puzzle_name + ".txt", "r")
-    size = int(file.readline())
+    row_size, col_size = get_array(file.readline())
     row_targets = np.array(get_array(file.readline()))
     col_targets = np.array(get_array(file.readline()))
     nums = []
-    for _ in range(size):
+    for _ in range(row_size):
         nums.append(get_array(file.readline()))
     nums = np.array(nums)
-    return size, row_targets, col_targets, nums
+    return row_size, col_size, row_targets, col_targets, nums
 
 def get_array(str_nums):
     return [int(i) for i in str_nums.split(',')]
 
-def solver(size, row_targets, col_targets, nums, verbose = False):
-    solution = np.array([[1 for _ in range(size)] for _ in range(size)])
-    guaranteed = np.array([[0 for _ in range(size)] for _ in range(size)])
+def solver(row_size, col_size, row_targets, col_targets, nums, verbose = False):
+    solution = np.array([[1 for _ in range(col_size)] for _ in range(row_size)])
+    guaranteed = np.array([[0 for _ in range(col_size)] for _ in range(row_size)])
+    return solver_helper(row_size, col_size, row_targets, col_targets, nums, solution, guaranteed, verbose)
 
-    #TODO: write a loop that recursively guesses until we get a solved puzzle or tries a new guess from the original solution if the recursive guesses lead to an invalid result, how do we optimize the guessing strategy to take advantage of probability of being correct
-    solved = solver_helper(size, row_targets, col_targets, nums, solution, guaranteed, verbose)
-    return solved
-
-def solver_helper(size, row_targets, col_targets, nums, solution, guaranteed, verbose):
+def solver_helper(row_size, col_size, row_targets, col_targets, nums, solution, guaranteed, verbose):
     has_changed = True 
 
     while has_changed:
@@ -39,18 +36,18 @@ def solver_helper(size, row_targets, col_targets, nums, solution, guaranteed, ve
             print("\n")
 
         curr_row_sums = np.sum(np.multiply(nums, solution), axis=1)
-        for r in range(size):
+        for r in range(row_size):
             if curr_row_sums[r] != row_targets[r]:
                 curr_row = nums[r,:] * (1 - guaranteed[r,:]) * solution[r,:]
                 curr_guaranteed = nums[r,:] * guaranteed[r,:]
 
                 guaranteed_set, not_used_set = combination_finder(curr_row[curr_row > 0], row_targets[r] - np.sum(curr_guaranteed))
-                for i in range(size):
+                for i in range(col_size):
                     if nums[r,i] in guaranteed_set and solution[r,i] != 0:
                         guaranteed[r,i] = 1
                         has_changed = True
                 
-                for i in range(size):
+                for i in range(col_size):
                     if nums[r,i] in not_used_set and guaranteed[r,i] == 0:
                         solution[r,i] = 0
                         has_changed = True
@@ -58,38 +55,37 @@ def solver_helper(size, row_targets, col_targets, nums, solution, guaranteed, ve
                 guaranteed[r,:] = solution[r,:]
 
         curr_col_sums = np.sum(np.multiply(nums, solution), axis=0)
-        for c in range(size):
+        for c in range(col_size):
             if curr_col_sums[c] != col_targets[c]:
                 curr_col = nums[:,c] * (1 - guaranteed[:,c]) * solution[:,c]
                 curr_guaranteed = nums[:,c] * guaranteed[:,c]
                 
                 guaranteed_set, not_used_set = combination_finder(curr_col[curr_col > 0], col_targets[c] - np.sum(curr_guaranteed))
-                for i in range(size):
+                for i in range(row_size):
                     if nums[i,c] in guaranteed_set and solution[i,c] != 0:
                         guaranteed[i,c] = 1
                         has_changed = True
 
-                for i in range(size):
+                for i in range(row_size):
                     if nums[i,c] in not_used_set and guaranteed[i,c] == 0:
                         solution[i,c] = 0
                         has_changed = True
             else:
                 guaranteed[:,c] = solution[:,c]
-                
-    if check_solution(size, row_targets, col_targets, solution, nums):
+    
+    if check_solution(row_targets, col_targets, solution, nums):
         return solution
     else:
-        for r in range(size):
-            for c in range(size):
+        for r in range(row_size):
+            for c in range(col_size):
                 if solution[r,c] == 1 and guaranteed[r,c] != 1:
                     solution_copy = solution.copy()
                     guaranteed_copy = guaranteed.copy()
                     solution_copy[r,c] = 0
-                    
-                    if check_valid(size, row_targets, col_targets, solution_copy, nums):
-                        return solver_helper(size, row_targets, col_targets, nums, solution_copy, guaranteed_copy, verbose)
+                    if check_valid(row_targets, col_targets, solution_copy, nums):
+                        return solver_helper(row_size, col_size, row_targets, col_targets, nums, solution_copy, guaranteed_copy, verbose)
 
-def check_valid(size, row_targets, col_targets, solution, nums):
+def check_valid(row_targets, col_targets, solution, nums):
     vals = nums * solution
     row = np.sum(vals, axis=1) - row_targets
     col = np.sum(vals, axis=0) - col_targets
@@ -97,7 +93,7 @@ def check_valid(size, row_targets, col_targets, solution, nums):
     b2 = (len(col[col < 0]) == 0)
     return b1 and b2
 
-def check_solution(size, row_targets, col_targets, solution, nums):
+def check_solution(row_targets, col_targets, solution, nums):
     vals = nums * solution
     row_sums = np.sum(vals, axis=1)
     col_sums = np.sum(vals, axis=0)
@@ -106,13 +102,12 @@ def check_solution(size, row_targets, col_targets, solution, nums):
     return b1 and b2
 
 if __name__ == "__main__":
-    size, row_targets, col_targets, nums = input("9-large-hard")
-    solved = solver(size, row_targets, col_targets, nums)
-     
-    print(size)
+    row_size, col_size, row_targets, col_targets, nums = input("10-rectangle")
+    solved = solver(row_size, col_size, row_targets, col_targets, nums)
+    
+    print("Finished Solving")
     print(row_targets)
     print(col_targets)
     print(nums)
-    print("\n")
     print(solved)
-    print(check_solution(size, row_targets, col_targets, solved, nums))
+    print(check_solution(row_targets, col_targets, solved, nums))
